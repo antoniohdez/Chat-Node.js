8a0614ff5367f04d903f04b83d3177bd83566f1f
Antonio <antonio.hdez93@gmail.com>
2013-11-13 17:07:25 -0600

    Asigna nombres aleatorios a los usuarios

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
     var net = require('net');
 
     var sockets = [];
+    var names = [];
 
     var s = net.Server(function(socket){
         sockets.push(socket);
-
+        names.push("user" + (Math.floor(Math.random()*10+1)).toString());
         socket.on('data', function(d){
             for(var i = 0; i < sockets.length; i++){
-            	if(sockets[i] == socket){ 
-            		continue;
-            	}
-                sockets[i].write(d);
+        	   if(sockets[i] === socket){ 
+        		  continue;
+                }
+                sockets[i].write(names[i] + ": " + d);
             }
         });
 
         socket.on('end', function(){
             var i = sockets.indexOf(socket);
             sockets.splice(i,2);
+            names.splice(i,2);
         });
 
     });
